9552204e875644eb680359004b4867e97edb563c
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-10 14:21:41 -0700

    added Homepage component. This is where all components will be imported on the homepage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import Header from './components/Header';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import Header from './Header'
+
+const Homepage = () =>{
+    return (
+        <div>
+            <Header/>
+            <h1>this is the homepage where everthing lives</h1>
+        </div>
+    )
+}
+
+export default Homepage;
