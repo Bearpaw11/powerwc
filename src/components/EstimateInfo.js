338f3333201a499774e1cf79dcc2b1c761395f39
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-21 21:52:46 -0700

    testing

--- src/components/EstimateInfo.js
+++ src/components/EstimateInfo.js
@@ -1,17 +1,42 @@
+import react, { useState } from "react";
 
-const EstimateInfo = () => {
-    
+
+
+const EstimateInfo = (props) => {
+    const [enteredAddress,setEnteredAddress] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+
+    const addressChangeHandler = (event) => {
+        setEnteredAddress(event.target.value)
+    };
+
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value)
+    };
+
+    const collectedInfo = (event) => {
+        event.preventDefault();
+        const collectedInfo = {
+            address: enteredAddress,
+            email: enteredEmail
+        };
+
+        props.onEnteredData(collectedInfo);       
+        setEnteredAddress('');
+        setEnteredEmail('');
+    }
+
     return (
         <div>
             <h3>Need a Quote for your driveway? Enter your address</h3>
-            <form>
+            <form onSubmit={collectedInfo}>
                 <div>
-                <label>Address </label>
-                <input type= 'text'></input>
+                    <label>Address </label>
+                    <input type='text' value={enteredAddress} onChange={addressChangeHandler}></input>
                 </div>
                 <div>
-                <label>Email </label>
-                <input type ='email'></input>
+                    <label>Email </label>
+                    <input type ='email' value={enteredEmail} onChange={emailChangeHandler}></input>
                 </div>
                 <div>
                     <button type="submit">Submit</button>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,11 +2,19 @@ import Header from './Header';
 import EstimateInfo from './EstimateInfo';
 
 const Homepage = () =>{
+    const saveInfoHandler = (collectedInfo) => {
+        const newInfo ={
+            ...collectedInfo,
+            id: Math.random().toString()
+        }
+        console.log(newInfo)
+    };
     return (
+        
         <div>
             <Header/>
-            <h1>Blah Blah Blah into</h1>
-            <EstimateInfo />
+            <h1>Blah Blah Blah into testing</h1>
+            <EstimateInfo onEnteredData={saveInfoHandler}/>
         </div>
     )
 }
